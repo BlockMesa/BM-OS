d2241df8eff6eee6c6f9225517baf368a90d70ae
Winwin <winwinlmfao@proton.me>
2024-03-31 07:21:31 +0000

    deleted:    .gitattributes 	deleted:    README.md 	deleted:    bios.lua 	deleted:    blank.lua 	deleted:    bootdisk.bat 	modified:   dos/.BOOT 	deleted:    dos/COMMAND.COM 	deleted:    dos/autoexec.bat 	deleted:    installer.lua 	deleted:    uninstall.bat 	deleted:    update.bat

--- bootdisk.bat
+++
@@ -1,8 +0,0 @@
-echo Boot disk creator/updater
-pause
-echo Updating .BOOT
-update-file disk/.BOOT https://raw.githubusercontent.com/BlockMesa/BM-DOS/main/dos/.BOOT
-echo Updating COMMAND.COM
-update-file disk/COMMAND.COM https://raw.githubusercontent.com/BlockMesa/BM-DOS/main/dos/COMMAND.COM
-echo Done!
-pause
