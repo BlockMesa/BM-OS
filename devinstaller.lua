c2f7ec96fe967d24aa14f799820c47a941d4913d
emir4169 <em.ireri41@gmail.com>
2024-04-26 18:16:38 +0300

    create a installer that should hopefully be up to date at all times

---
+++ devinstaller.lua
@@ -0,0 +1,95 @@
+local makeJson = textutils.serializeJSON
+local makeTable = textutils.unserializeJSON
+
+local a = http.get('https://restructsblog.neocities.org/devmeta.json')
+local c = textutils.unserializeJSON(a.readAll())
+local b = c.packages.base
+local b1 = c.packages.kernel
+local b2 = c.packages.bootloader
+local b3 = c.packages.shell
+local b4 = c.packages.package
+
+a.close()
+local baseUrl = b.assetBase
+local baseUrl1 = b1.assetBase
+local baseUrl2 = b2.assetBase
+local baseUrl3 = b3.assetBase
+local baseUrl4 = b4.assetBase
+print("BM-OS WILL NOW BE INSTALLED.")
+fs.makeDir("/home")
+fs.makeDir("/bin")
+fs.makeDir("/sbin")
+fs.makeDir("/etc")
+fs.makeDir("/usr")
+fs.makeDir("/lib")
+fs.makeDir("/usr/bin")
+fs.makeDir("/usr/lib")
+fs.makeDir("/usr/bin")
+fs.makeDir("/usr/etc")
+
+local file = fs.open("/etc/packages.d/packages.json","w")
+file.write(makeJson({
+    updated = "",
+    installed = {
+        bios = {
+            packageId = "bios",
+            version = c.packages.bios.version,
+        },
+        base = {
+            packageId = "base",
+            version = b.version,
+        },
+        kernel = {
+            packageId = "kernel",
+            version = b1.version,
+        },
+        bootloader = {
+            packageId = "bootloader",
+            version = b2.version,
+        },
+        shell = {
+            packageId = "shell",
+            version = b3.version,
+        },
+        package = {
+            packageId = "package",
+            version = b4.version,
+        }
+    }
+}))
+file.close()
+
+local function installFile(url,file)
+    local result, reason = http.get({url = url, binary = true}) --make names better
+    if not result then
+        print(("Failed to update %s from %s (%s)"):format(file, url, reason)) --include more detail
+        return
+    end
+    a1 = fs.open(file,"wb")
+    a1.write(result.readAll())
+    a1.close()
+    result.close()
+end
+print("Installing kernel")
+for i,v in pairs(b1.files) do
+    installFile(baseUrl1..v,v)
+end
+print("Installing bootloader")
+for i,v in pairs(b2.files) do
+    installFile(baseUrl2..v,v)
+end
+print("Installing shell")
+for i,v in pairs(b3.files) do
+    installFile(baseUrl3..v,v)
+end
+print("Installing package manager")
+for i,v in pairs(b4.files) do
+    installFile(baseUrl4..v,v)
+end
+print("Installing base commands")
+for i,v in pairs(b.files) do
+    installFile(baseUrl..v,v)
+end
+installFile(c.packages.bios.assetBase.."bios.lua","startup.lua")
+print("Developer Installation complete!")
+os.reboot()
